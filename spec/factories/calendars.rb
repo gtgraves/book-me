84f4ae108963a057489ff456935fa4ebcd210c5b
George Graves <gtgraves@gmail.com>
2018-02-04 11:54:31 -0600

    Added Calendar Model

---
+++ spec/factories/calendars.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :calendar do
+    title Faker::Superhero.power
+    description Faker::Friends.quote
+    user 
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name Faker::Name.name
+    email Faker::Internet.email
+    password "helloworld"
+    password_confirmation "helloworld"
+    confirmed_at Date.today
   end
 end
